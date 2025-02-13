eb259f45a294957e72fbdf1080790589f6032869
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-13 16:02:34 -0300

    Criado o metodo listarAlunos na classe controller para as solicitacoes HTTP GET para listar todos os alunos

--- src/main/java/com/zup/escola/controller/AlunoController.java
+++ src/main/java/com/zup/escola/controller/AlunoController.java
@@ -1,12 +1,24 @@
 package com.zup.escola.controller;
 
+import com.zup.escola.model.Aluno;
 import com.zup.escola.service.AlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/alunos")
 public class AlunoController {
 
+    @Autowired
     private AlunoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Aluno>> listarAlunos(){
+        return ResponseEntity.ok().body(service.listarAlunos());
+    }
 }
