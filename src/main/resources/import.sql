27037eeb9244d2af1c4be1e6339655f860527a96
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-13 16:42:25 -0300

    SQL com os dados para implementar quando for rodar a API.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO alunos (nome, idade) VALUES ('Wendell', 31);
+INSERT INTO alunos (nome, idade) VALUES ('Baresi', 31);
+INSERT INTO alunos (nome, idade) VALUES ('Ana', 20);
+INSERT INTO alunos (nome, idade) VALUES ('Lucia', 25);
