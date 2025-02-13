f9c5a37e9767a076b6957e538a6cdd73ae17efee
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-13 15:46:45 -0300

    Criado a classe AlunoService dentro do pacote service para encapsular toda a logica de negocios relacionada a classe modelo Aluno

---
+++ src/main/java/com/zup/escola/service/AlunoService.java
@@ -0,0 +1,16 @@
+package com.zup.escola.service;
+
+import com.zup.escola.model.Aluno;
+import com.zup.escola.repository.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository repository;
+
+}
