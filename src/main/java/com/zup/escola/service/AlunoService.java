b2aad5bbbb84de1dd664b17ef44d99f2dbd178f5
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-13 16:31:15 -0300

    Metodo listarAlunos foi alterado receber agora um AlunoResponseDTO que vai retornar uma lista de objetos no formato da API. O metodo vai recuperar os dados do repositorio e em seguida o metodo vai operar para um map e coverter os dados para o objeto Aluno.

--- src/main/java/com/zup/escola/service/AlunoService.java
+++ src/main/java/com/zup/escola/service/AlunoService.java
@@ -1,11 +1,13 @@
 package com.zup.escola.service;
 
+import com.zup.escola.dtos.AlunoResponseDTO;
 import com.zup.escola.model.Aluno;
 import com.zup.escola.repository.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AlunoService {
@@ -13,7 +15,8 @@ public class AlunoService {
     @Autowired
     private AlunoRepository repository;
 
-    public List<Aluno> listarAlunos(){
-        return repository.findAll();
+    public List<AlunoResponseDTO> listarAlunos(){
+        return repository.findAll().stream().map(aluno -> new AlunoResponseDTO(aluno.getId(), aluno.getNome(), aluno.getIdade()))
+                .collect(Collectors.toList());
     }
 }
