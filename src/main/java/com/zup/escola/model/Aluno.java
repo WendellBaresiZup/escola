fbe90075184593d4a58e42bdac3ee9d46a9ce17d
Wendell Baresi <wendell.oliveira@zup.com.br>
2025-02-13 15:35:33 -0300

    Passado as anotations dentro da classe

--- src/main/java/com/zup/escola/model/Aluno.java
+++ src/main/java/com/zup/escola/model/Aluno.java
@@ -1,7 +1,48 @@
 package com.zup.escola.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "alunos")
 public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column
     private String nome;
+    @Column
     private Integer idade;
+
+    public Aluno() {
+    }
+
+    public Aluno(Long id, String nome, Integer idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
